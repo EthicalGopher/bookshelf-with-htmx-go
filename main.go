d66d919cc13ca9e47b81cfe4de50e36489b81ab1
Sankhyahrick Swami <sankhyahricks@gmail.com>
2025-01-11 00:32:41 +0530

    3rd push

--- main.go
+++ main.go
@@ -1,64 +1,77 @@
 package main
 
 import (
-	
+	"fmt"
 	"io"
 	"os"
 	"strings"
-	
+
 	"github.com/gofiber/fiber/v2"
 )
+
 func checkError(err error) {
 	if err != nil {
-		panic(err)
+		fmt.Println("Error:", err)
 	}
 }
+
+
 func BookShelf() []string {
-	file,err:=os.Open("books.txt")
+	file, err := os.Open("books.txt")
 	checkError(err)
 	defer file.Close()
-	arr,err:=io.ReadAll(file)
+
+	arr, err := io.ReadAll(file)
 	checkError(err)
-	var Books []string
+
 	lines := strings.Split(string(arr), "\n")
-	
-	Books=append(Books, lines...)
+	var Books []string
+
+	for _, line := range lines {
+		if line != "" {
+			Books = append(Books, line)
+		}
+	}
+
 	return Books
 }
+
 func main() {
-	app:=fiber.New()
-	app.Get("/",func(c * fiber.Ctx)error{
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendFile("./index.html")
 	})
-	app.Get("/search",func(c *fiber.Ctx)error{
-		query:=c.Query("search"," ")
-		if query == "" {
-			return c.SendString("Please enter the name of the book to search for it")
-		}
+
+
+	app.Get("/search", func(c *fiber.Ctx) error {
+		query := c.Query("search", "")
 		var result []string
-		Books:=BookShelf()
-		for _,j:= range Books {
-			if strings.Contains(strings.ToLower(j), strings.ToLower(query)){
-				result=append(result, j)
+		Books := BookShelf()
+
+		for _, book := range Books {
+			if strings.Contains(strings.ToLower(book), strings.ToLower(query)) {
+				result = append(result, book)
 			}
 		}
 
-		if len(result)==0 {
-			return c.SendString("Sorry can't find the book you mentioned ("+query+")")
+		if len(result) == 0 {
+			return c.SendString("Sorry, can't find the book you mentioned (" + query + ")")
 		}
 
-		html:="<ul>"
-		for _,j:=range result{
-			html+="<li>"+j+"</li>"
+		html := "<ul>"
+		for i := 0; i < len(result) && i < 5; i++ {
+			html += "<li>" + result[i] + "</li>"
 		}
-		html+="</ul>"
-		if query != ""{
-
+		html += "</ul>"
+		if query!="" && query!=" "{
 			return c.SendString(html)
 		}
-		return c.SendString("Please enter the name of the book to search for it")
+		return c.SendString("Please enter the query")
 	})
 
+
+
 	app.Listen(":8000")
-	
+
 }
